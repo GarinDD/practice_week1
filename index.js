bca6e7a2058171b05229cf9304c26eb23f3a5ef8
Daniel` <streetbossog@gmail.com`>
2024-01-29 19:29:53 +0300

    1`

---
+++ index.js
@@ -0,0 +1,20 @@
+const obj = {
+    name : `asd`,
+    age: 20,
+    surename : undefined
+}
+
+const arr = [`name`, `age`]
+
+obj[`name`] == obj.name
+
+obj[`surname`]
+
+console.log(`surname` in obj)
+console.log(`surename1` in obj)
+
+
+
+for (let el of arr) {
+    console.log(el, obj[el])
+}
